3a3741ece85f6331b8f905bc5e72a77a53764198
BladedElm04 <joaogui1905@hotmail.com>
2024-02-24 00:52:41 -0300

    feat: cors and tsconfig for deploy

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { tasksRouter, categoriesRouter, userRouter } from "./routes";
 import { HandleErrosMiddleware } from "./middlewares/HandleErrors.middleware";
 
 export const app = express();
 
 app.use(json());
+app.use(cors())
 app.use(helmet());
 
 app.use("/tasks", tasksRouter);
